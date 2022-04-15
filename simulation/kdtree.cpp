fdd692c71ec3ec495741f11f91b4c7caed4e7996
nr1040 <nicholasrichards97@gmail.com>
2022-04-15 09:10:38 -0400

    WIP mergesort and kdtree

--- simulation/kdtree.cpp
+++ simulation/kdtree.cpp
@@ -12,33 +12,33 @@ using namespace std;
 struct Node
 {
 public:
-    Particle val;
+    vector<Particle> val;
     Node *left;
     Node *right;
     Node *back;
 
-    Node(Particle p)
+    Node(vector<Particle> p)
     : val(p)
     , left(nullptr)
     , right(nullptr)
     , back(nullptr)
     {}
 
-    Node(Particle p, Node *left)
+    Node(vector<Particle> p, Node *left)
     : val(p)
     , left(left)
     , right(nullptr)
     , back(nullptr)
     {}
 
-    Node(Particle p, Node *left, Node *right)
+    Node(vector<Particle> p, Node *left, Node *right)
     : val(p)
     , left(left)
     , right(right)
     , back(nullptr)
     {}
 
-    Node(Particle p, Node *left, Node *right, Node *back)
+    Node(vector<Particle> p, Node *left, Node *right, Node *back)
     : val(p)
     , left(left)
     , right(right)
@@ -48,13 +48,13 @@ public:
 
     bool isRoot()
     {
-
+        return 0;
     }
 
 
     bool isLeaf()
     {
-
+        return 0;
     }
 
 
@@ -63,16 +63,37 @@ public:
 
 //Templatize input container
 //Add sort algorithm
-class KDTree
+//k-d tree space partition, as tree is being built, 
+//  return possible collision pairs (leafs where vector size > 1)
+// pre-sort points vector
+//eventually return start and end index instead of storing vectors as value
+//  saves memory
+class KDTree_SP
 {
 private:
-    vector<double> points;
+    vector<Particle> points;
+    vector < vector<Particle> > possible_collisions;
+    vector<Particle> x_sorted; //when we switch to storing start and end indexes
+    vector<Particle> y_sorted; //when we switch to storing start and end indexes
 
 public:
 
-    KDTree(vector<double> points)
+    KDTree_SP(vector<Particle> points)
     : points(points)
-    {}
+    {
+        Node* root = new Node(points);
+
+        build(root);
+    }
+
+
+    vector< vector<double> > build(Node* n)
+    {
+        vector< vector<double> > val;
+
+
+        return val;
+    }
 
 
 
@@ -80,3 +101,103 @@ public:
 
 
 };
+
+
+
+
+
+// It divides the input array into two halves, 
+//calls itself for the two halves, 
+//and then merges the two sorted halves. 
+//The merge() function is used for merging two halves. 
+//The merge(arr, l, m, r) is a key process that assumes that arr[l..m] and arr[m+1..r] 
+//are sorted and merges the two sorted sub-arrays into one
+//https://www.geeksforgeeks.org/merge-sort/
+//https://en.wikipedia.org/wiki/Merge_sort
+//raise error if axis is not 0 or 1
+vector<Particle> mergeSort(vector<Particle> p, int axis)
+{
+    vector<Particle> left;
+    vector<Particle> right;
+
+    int size = p.size();
+    int mid_index = size/2;
+
+    if (size <= 1)
+        return p;
+
+    for(int i=0; i < size; i++)
+    {
+        if (i <= mid_index)
+        {
+            left.push_back(p[i]);
+        }
+        else
+        {
+            right.push_back(p[i]);
+        }
+    }
+
+    left = mergeSort(left, axis);
+    right = mergeSort(right, axis);
+
+    return merge(left,right,axis);
+
+}
+
+
+vector<Particle> merge(vector<Particle> left, vector<Particle> right, int axis)
+{
+    vector<Particle> result;
+
+    double left_value, right_value;
+
+    while (left.size() > 0 && right.size() > 0)
+    {
+        if (axis == 0)
+        {   
+            left_value = left[0].getXpos();
+            right_value = right[0].getXpos();
+        }
+        else
+        {
+            left_value = left[0].getYpos();
+            right_value = right[0].getYpos();
+        }
+        
+        if (left_value <= right_value)
+        {
+            result.push_back(left[0]);
+            left.erase(left.begin());
+        }
+        else
+        {
+            result.push_back(right[0]);
+            right.erase(right.begin());
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+/*
+class MergeSort
+{
+private:
+    void merge()
+    {
+
+    }
+
+
+
+public:
+    MergeSort(vector<Particle> particles);
+};
+*/
